1e8543fc7a9b8d7992fc8190dab7cee8db01456c
Walter Alejandro Duchi Rivera <waltduchi@gmail.com>
2024-11-04 20:39:23 -0500

    vinculado navbar a cada componente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,34 @@
 import { Routes } from '@angular/router';
 import { RegistroPropuestasComponent } from './registro-propuestas/registro-propuestas.component';
+import { AsignacionRevisoresComponent } from './asignacion-revisores/asignacion-revisores.component';
+import { PlanificacionPeriodoComponent } from './planificacion-periodo/planificacion-periodo.component';
+import { CargarPropuestasComponent } from './cargar-propuestas/cargar-propuestas.component';
+import { CorrecionEstudianteComponent } from './correcion-estudiante/correcion-estudiante.component';
+import { CreacionComisionComponent } from './creacion-comision/creacion-comision.component';
 
 export const routes: Routes = [
-  // ... otras rutas existentes ...
   {
     path: 'registro-propuesta',
     component: RegistroPropuestasComponent
+  },
+  {
+    path: 'asignacion-revisores',
+    component: AsignacionRevisoresComponent
+  },
+  {
+    path: 'planificacion-periodo',
+    component: PlanificacionPeriodoComponent
+  },
+  {
+    path: 'cargar-propuestas',
+    component: CargarPropuestasComponent
+  },
+  {
+    path: 'correcion-estudiante',
+    component: CorrecionEstudianteComponent
+  },
+  {
+    path: 'creacion-comision',
+    component: CreacionComisionComponent
   }
 ];
